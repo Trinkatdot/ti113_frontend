deb8f98f716c1586b0309b9362cd11929192409c
Trinkatdot <agatha.ktaureliano@senacsp.edu.br>
2025-02-10 22:10:19 -0300

    feat: correção do js

--- ex5correção/script.js
+++ ex5correção/script.js
@@ -7,11 +7,28 @@ btnRegister.addEventListener("click", () => {
     let password = document.querySelector("#txtPassword").value;
     let ConfirmPassword = document.querySelector("#txtConfirmPassword").value;
 
+    let listaUsuarios = [];
+    
     if (username.trim() !== ""
         && email.trim() !== ""
         && password.trim() !== ""
         && ConfirmPassword.trim() !== "") {
-            alert("Todos os campos devem ser preenchidos")
+            alert("Todos os campos devem ser preenchidos");
+            return;
+    }
+
+    if (password.trim() !== ConfirmPassword.trim()) {
+        alert("As Senhas não correspondem.");
+        return;
+    }
+
+    listaUsuarios = JSON.parse(localStorage.getItem('listaUsuarios'));
+
+    if(listaUsuarios.length > 0) {
+        if (listaUsuarios.some(user => user.email == email)) {
+            alert('Email já cadastrado');
+            return;
+        }
     }
 
 });
